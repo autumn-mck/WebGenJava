c56b81de400c9da781b48f329ed40b6695f7c23e
Autumn McKee <autumn@mck.is>
2022-04-30 21:33:56 +0100

    Also ignore README.md files

--- src/webGen/BuildOptions.java
+++ src/webGen/BuildOptions.java
@@ -15,9 +15,9 @@ public class BuildOptions {
 	// List of files to ignore
 	// Note: Currently only very basic pattern matching, eg will ignore all files/folders containing `.git`
 	private static List<String> ignoredFiles = Arrays.asList(
-			".git");
+			".git", "README.md");
 	
-	// The line used to mark the boundry between information about the post and the post itself
+	// The line used to mark the boundary between information about the post and the post itself
 	private static String postStartSeperator = "-----";
 
 	// Getters for everything in this class
